d405ae441a4c8f89ecb1161014f0717b1af9f048
sebastianmorenogutierrez <morenogutierrezjuansebastian@gmail.com>
2025-09-08 23:29:15 -0500

    first commit

--- src/main/java/com/example/domain/JugadorDTO.java
+++ src/main/java/com/example/domain/JugadorDTO.java
@@ -1,4 +1,32 @@
 package com.example.domain;
 
 public class JugadorDTO {
+    private String nombre;
+    private int partidosGanados;
+    private int partidosJugados;
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPartidosGanados() {
+        return partidosGanados;
+    }
+
+    public void setPartidosGanados(int partidosGanados) {
+        this.partidosGanados = partidosGanados;
+    }
+
+    public int getPartidosJugados() {
+        return partidosJugados;
+    }
+
+    public void setPartidosJugados(int partidosJugados) {
+        this.partidosJugados = partidosJugados;
+    }
 }
